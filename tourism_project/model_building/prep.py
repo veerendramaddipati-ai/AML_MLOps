38e4729ba2365e8e93eef96880918bd64308ee94
veerendramaddipati-ai <veerendra.maddipati@gmail.com>
2026-08-07 13:24:01 +0530

    add tourism_project/model_building/prep.py

---
+++ tourism_project/model_building/prep.py
@@ -0,0 +1,33 @@
+
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+
+DATA_PATH = "tourism_project/data/tourism.csv"
+
+df = pd.read_csv(DATA_PATH)
+
+# Remove unnecessary identifier column
+df.drop(columns=["CustomerID"], inplace=True)
+
+# Save complete dataset structure
+target = "ProdTaken"
+
+X = df.drop(target, axis=1)
+y = df[target]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    stratify=y,
+    test_size=0.20,
+    random_state=42
+)
+
+X_train.to_csv("Xtrain.csv", index=False)
+X_test.to_csv("Xtest.csv", index=False)
+
+y_train.to_csv("ytrain.csv", index=False)
+y_test.to_csv("ytest.csv", index=False)
+
+print("Train Test Split Saved Successfully")
